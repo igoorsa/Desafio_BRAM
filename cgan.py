c80e581a14248ddfd52da9eab28c3ba7ef8171f4
Igor Alves <igor8.alves@hotmail.com>
2024-01-22 10:26:04 -0300

    update models

--- cgan.py
+++ cgan.py
@@ -1,8 +1,9 @@
 from __future__ import print_function, division
 
 from keras.layers import Input, Dense, Dropout
-from keras.layers import LSTM, Activation
-
+from keras.layers import LSTM, Activation, BatchNormalization, LeakyReLU
+from keras.initializers import he_normal
+from keras.regularizers import l2
 from keras.models import Sequential
 from keras.optimizers import Adam, RMSprop
 
@@ -10,10 +11,11 @@ import numpy as np
 
 
 class Modelos_DGAN(object):
-    def __init__(self, look_back, vars, var_y):
+    def __init__(self, look_back, var, var_y):
+        
 
         self.look_back = look_back      
-        self.vars = vars  
+        self.var = var  
         self.var_y = var_y  
         self.esqueleto_D = None  
         self.esqueleto_G = None 
@@ -27,7 +29,7 @@ class Modelos_DGAN(object):
         
         self.esqueleto_D = Sequential()
 
-        self.esqueleto_D.add(Dense(128, input_dim=self.var_y, activation='relu', kernel_initializer='he_normal'))
+        self.esqueleto_D.add(Dense(2, input_dim=self.var_y, activation='relu', kernel_initializer='he_normal'))
         self.esqueleto_D.add(Dropout(0.5))  
         self.esqueleto_D.add(Dense(256, activation='relu', kernel_initializer='he_normal'))
         self.esqueleto_D.add(Dense(1, activation='sigmoid'))
@@ -37,48 +39,54 @@ class Modelos_DGAN(object):
     def esqueleto_generator(self):
         if self.esqueleto_G:
             return self.esqueleto_G
+        
         self.esqueleto_G = Sequential()
-        self.esqueleto_G.add(LSTM(64, input_shape=(self.look_back, self.vars), dropout=0.2, recurrent_dropout=0.2))
+        
+        self.esqueleto_G.add(LSTM(64, input_shape=(self.look_back, self.var), dropout=0.2, recurrent_dropout=0.2))
         self.esqueleto_G.add(Dense(32, activation='relu', kernel_regularizer='l2'))
-        self.esqueleto_G.add(Dropout(0.5))
-        self.esqueleto_G.add(Dense(self.var_y, activation='linear'))  
+        self.esqueleto_G.add(Dropout(0.5)) 
+        
+        self.esqueleto_G.add(Dense(self.var_y, activation='linear')) 
 
         return self.esqueleto_G
 
     def modelo_discriminator(self): 
         if self.modelo_D:
             return self.modelo_D
-        optimizer = Adam(lr=0.00002)
+        optimizer = Adam(lr=0.0002)
         self.modelo_D = Sequential()
         self.modelo_D.add(self.esqueleto_discriminator())
-        self.modelo_D.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+        self.modelo_D.compile(loss='binary_crossentropy', optimizer=optimizer)
         return self.modelo_D 
 
     def modelo_adversarial(self): 
         if self.modelo_A:
             return self.modelo_A
-        optimizer = Adam(lr=0.00001)
+        optimizer = RMSprop(lr=0.0001)
         self.modelo_A = Sequential()
         self.modelo_A.add(self.esqueleto_generator())
         self.modelo_A.add(self.esqueleto_discriminator())
-        self.modelo_A.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+        self.modelo_A.compile(loss='mean_squared_error', optimizer=optimizer)
         return self.modelo_A
     
 
 
 
-class DCGAN(object):
-    def __init__(self, x_treino=None, look_back = 3, vars = None, var_y= None):
+class DGAN(object):
+    def __init__(self, x_treino=None, look_back = 3, var = None, var_y= None):
         self.look_back = look_back
 
+        self.loss_dis = []
+        self.loss_adv = []
+
         self.x_treino = x_treino[:,3]
-        self.vars = vars
+        self.var = var
         self.var_y = var_y
 
-        self.modelos_DCGAN = Modelos_DGAN(look_back, vars, var_y)
-        self.discriminator =  self.modelos_DCGAN.modelo_discriminator()
-        self.adversarial = self.modelos_DCGAN.modelo_adversarial()
-        self.generator = self.modelos_DCGAN.esqueleto_generator()
+        self.modelos_DGAN = Modelos_DGAN(look_back, var, var_y)
+        self.discriminator =  self.modelos_DGAN.modelo_discriminator()
+        self.adversarial = self.modelos_DGAN.modelo_adversarial()
+        self.generator = self.modelos_DGAN.esqueleto_generator()
         
     
     def discriminator_trainable(self, val): 
@@ -99,9 +107,9 @@ class DCGAN(object):
 
         for i in range(train_steps):
             
-            dados_reais = self.x_treino[np.random.randint(0, self.x_treino.shape[0], size=batch_size), ] 
+            dados_reais = self.x_treino[np.random.randint(0, self.x_treino.shape[0], size=batch_size), ]
             dados_reais = dados_reais.reshape((dados_reais.shape[0],1))
-            ruidos = np.random.uniform(0, 1.0, size=[batch_size+self.look_back+1, self.vars]) 
+            ruidos = np.random.uniform(0, 1.0, size=[batch_size+self.look_back+1, self.var]) 
             ruidos = self.create_dataset(ruidos,self.look_back)
             dados_fake = self.generator.predict(ruidos) 
             x = np.concatenate((dados_reais, dados_fake)) 
@@ -116,15 +124,19 @@ class DCGAN(object):
 
             
             y = np.ones([batch_size, 1])
-            ruidos = np.random.uniform(0, 1.0, size=[batch_size+self.look_back+1, self.vars]) 
+            ruidos = np.random.uniform(0, 1.0, size=[batch_size+self.look_back+1, self.var]) 
             ruidos = self.create_dataset(ruidos,self.look_back)
             self.discriminator_trainable(False)
             loss_adversarial = self.adversarial.train_on_batch(ruidos, y)
+
+            self.loss_dis.append(loss_discriminator)
+            self.loss_adv.append(loss_adversarial)
             
                
-            print('%d: [Discriminator loss: %f, acc: %f]   [Adversarial loss: %f, acc: %f]'  % (i, loss_discriminator[0], 
-                                                                                                loss_discriminator[1], 
-                                                                                                loss_adversarial[0], 
-                                                                                                loss_adversarial[1]))
+            print('%d: [Discriminator loss: %f]   [Adversarial loss: %f]'  % (i, loss_discriminator, loss_adversarial))
+        loss = {}
+        loss['discriminator'] = self.loss_dis
+        loss['adversarial']   = self.loss_adv
+        return loss
             
     
